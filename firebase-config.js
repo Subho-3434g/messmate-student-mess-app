55692aade5f4740e1eac4ae9f564facbb3706600
Subho-3434g <subho1212g@gmail.com>
2026-05-03 14:39:40 +0530

    Update firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.MESSMATE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDMZV2BNhZ3iNjqHzg5nEPJS3yVyin_Eu4",
+  authDomain: "bhawalmess.firebaseapp.com",
+  projectId: "bhawalmess",
+  storageBucket: "bhawalmess.firebasestorage.app",
+  messagingSenderId: "846631870273",
+  appId: "1:846631870273:web:8da6ca426c5864fa910bc0",
+  measurementId: "G-EWMVN5ES8W"
 };
